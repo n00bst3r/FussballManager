8c4fa3851412c994716b4fe528480d6d87b42215
Daniel Rajic <kontakt@danielrajic.de>
2014-04-25 00:00:15 +0200

    Klasse LeagueModus, Interface LeaguePlayable

---
+++ de/danielrajic/interfaces/LeaguePlayable.java
@@ -0,0 +1,15 @@
+package de.danielrajic.interfaces;
+
+public interface LeaguePlayable {
+	
+	public String getHomeTeam();
+	
+	public String getAwayTeam();
+	
+	public int getScoreHome();
+	
+	public int getScoreAway();
+	
+	public String getResultText();
+
+}
